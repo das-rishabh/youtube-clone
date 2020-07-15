cfd00fcbe5208b8b3175cc20cab181fa66c82281
Rishabh Das <rishabhdasufl@gmail.com>
2020-07-16 00:13:01 +0530

    Add component VideoDetail

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "./apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 const KEY = "AIzaSyDcpUks4W-MtemF3B23-_Jrs8yjfIvy0Tw";
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
@@ -18,11 +19,19 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }

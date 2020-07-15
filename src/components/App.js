58c7b7f2b4f8bcf9c747d211eb44135f5c1e91c7
Rishabh Das <rishabhdasufl@gmail.com>
2020-07-15 20:05:58 +0530

    Hook youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "./apis/youtube";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+const KEY = "AIzaSyDcpUks4W-MtemF3B23-_Jrs8yjfIvy0Tw";
+class App extends React.Component {
+  onTermSubmit = (term) => {
+    youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
